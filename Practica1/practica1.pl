7a34b3272e9b9603494c4aa28ec6be2683b067a7
palbertito <palbertito@hotmail.com>
2019-03-27 11:24:40 +0100

    Comentados Nombres

--- Practica1/practica1.pl
+++ Practica1/practica1.pl
@@ -1,7 +1,7 @@
 %Nuestros datos
-alumno_prode(Alvarez,Benavente,Alejandro,y160319).
-alumno_prode(Pascual,Cristobal,Alejandro,y160336).
-alumno_prode(Aparicio,Doncel,Alberto,y160364).
+%alumno_prode(Alvarez,Benavente,Alejandro,y160319).
+%alumno_prode(Pascual,Cristobal,Alejandro,y160336).
+%alumno_prode(Aparicio,Doncel,Alberto,y160364).
 %-------------------------------------------
 
 % practica1.pl
@@ -9,6 +9,7 @@ alumno_prode(Aparicio,Doncel,Alberto,y160364).
 % Números de Peano
 nat(0).
 nat(s(N)):-nat(s(N)).
+
 
 %Primera Parte
 
