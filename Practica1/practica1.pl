da3cee81c142bd2c763d32998ef561ed1165809c
palbertito <palbertito@hotmail.com>
2019-03-31 14:50:14 +0200

    esTorre bien

--- Practica1/practica1.pl
+++ Practica1/practica1.pl
@@ -9,44 +9,44 @@
 
 % Números de Peano
 nat(0).
-nat(s(N)):-nat(s(N)).
+nat(s(N)) :-
+    nat(s(N)).
 
-nat_geq(0,0).
-nat_geq(N,0) :-
-	nat(N),
-	N \= 0.
-nat_geq(s(N),s(M)) :-
-	nat_geq(N,M).
+nat_geq(0, 0).
+nat_geq(N, 0) :-
+    nat(N),
+    N\=0.
+nat_geq(s(N), s(M)) :-
+    nat_geq(N, M).
 
-nat_leq(0,0).
-nat_leq(0,N) :-
-	nat(N),
-	N \= 0.
-nat_leq(s(N),s(M)) :-
-	nat_leq(N,M).
+nat_leq(0, 0).
+nat_leq(0, N) :-
+    nat(N),
+    N\=0.
+nat_leq(s(N), s(M)) :-
+    nat_leq(N, M).
 
-nat_lt(0,N) :-
-	nat(N),
-	N \= 0.
-nat_lt(s(N),s(M)) :-
-	nat_lt(N,M).
+nat_lt(0, N) :-
+    nat(N),
+    N\=0.
+nat_lt(s(N), s(M)) :-
+    nat_lt(N, M).
 
-nat_gt(N,0) :-
-	nat(N),
-	N \= 0.
-nat_gt(s(N),s(M)) :-
-	nat_gt(N,M).
+nat_gt(N, 0) :-
+    nat(N),
+    N\=0.
+nat_gt(s(N), s(M)) :-
+    nat_gt(N, M).
 
 %Primera Parte
 
 %pieza(Ancho, Alto, Prof, Color).
-
-pieza(nat(N), nat(N), nat(N), Colour):-
-    nat_geq(N,0),
+pieza(nat(N), nat(N), nat(N), Colour) :-
+    nat_geq(N, 0),
     member(Colour, [am, r, a, v]).
 
-piezaTorre(nat(N), _ , nat(N), Colour):-
-    nat_geq(N,0),
+piezaTorre(nat(N), _, nat(N), Colour) :-
+    nat_geq(N, 0),
     member(Colour, [am, r, a, v]).
     
 
@@ -56,24 +56,22 @@ piezaTorre(nat(N), _ , nat(N), Colour):-
 
 %esTorre(Construccion).
 
-prefix([],Ys).
-prefix([X|Xs],[X|Ys]) :-
-	prefix(Xs,Ys).
-
-sublist_recursive(Xs,Ys) :-
-	prefix(Xs,Ys).
-sublist_recursive(Xs,[_|Ys]) :-
-	sublist_recursive(Xs,Ys).
-
 %esTorre(pieza(s(0),s(0),s(0),_)).
+esTorre([X, Xs]) :-
+    arg(1, X, Value1),
+    arg(1, Xs, Value2),
+    arg(3, X, Value3),
+    arg(3, Xs, Value4),
+    nat_leq(Value1, Value2),
+    nat_leq(Value3, Value4).
 
-esTorre([X,Xs]):-
-    arg(1, X, Value)=arg(1,Xs,Value),
-    arg(3, X, Value)=arg(3,Xs,Value).
-
-esTorre([X,Xs|Y]):-
-    arg(1, X, Value)=arg(1,Xs,Value),
-    arg(3, X, Value)=arg(3,Xs,Value),
+esTorre([X, Xs|Y]) :-
+    arg(1, X, Value1),
+    arg(1, Xs, Value2),
+    arg(3, X, Value3),
+    arg(3, Xs, Value4),
+    nat_leq(Value1, Value2),
+    nat_leq(Value3, Value4),
     esTorre([Xs|Y]).
 
 
