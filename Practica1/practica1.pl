853e78ffcf8d25d2a1813a49adf4de79a4db2b22
palbertito <palbertito@hotmail.com>
2019-04-02 13:09:32 +0200

    >= > <= < arreglados

--- Practica1/practica1.pl
+++ Practica1/practica1.pl
@@ -20,31 +20,21 @@ eq(0, 0).
 eq(s(A), s(B)) :-
     eq(A, B).
 
-nat_geq(0, 0).
-nat_geq(N, 0) :-
-    nat(N),
-    N\=0.
-nat_geq(s(N), s(M)) :-
-    nat_geq(N, M).
+grt_eq(_N, zero).					% Every natural number is larger than 0
+grt_eq(s(N), s(M)):- 				% a > b => (a + 1) > (b + 1) 
+	grt_eq(N, M).
 
-nat_leq(0, 0).
-nat_leq(0, N) :-
-    nat(N),
-    N\=0.
-nat_leq(s(N), s(M)) :-
-    nat_leq(N, M).
+grt(_N, 0).					    % Every natural number is larger than 0
+grt(s(N), s(M)):- 				% a > b => (a + 1) > (b + 1) 
+	grt(N, M), N \= M.
 
-nat_lt(0, N) :-
-    nat(N),
-    N\=0.
-nat_lt(s(N), s(M)) :-
-    nat_lt(N, M).
+lst_eq(0, _).					    % 0 is the smallest natural number
+lst_eq(s(N), s(M)):-				% a < b => (a + 1) < (b + 1) 
+	lst_eq(N, M).
 
-nat_gt(N, 0) :-
-    nat(N),
-    N\=0.
-nat_gt(s(N), s(M)) :-
-    nat_gt(N, M).
+lst(0, _).					    % 0 is the smallest natural number
+lst(s(N), s(M)):-				% a < b => (a + 1) < (b + 1) 
+	lst(N, M), N \= M.
 
 sum(0, A, A).
 sum(s(A), B, s(C)) :-
@@ -84,11 +74,11 @@ peano_par(s(s(X))) :-
     peano_par(X).
 
 pieza(nat(N), nat(N), nat(N), Colour) :-
-    nat_geq(N, 0),
+    grt_eq(N, 0),
     member(Colour, [am, r, a, v]).
 
 piezaTorre(nat(N), _, nat(N), Colour) :-
-    nat_geq(N, 0),
+    grt_eq(N, 0),
     member(Colour, [am, r, a, v]).
 
 %esTorre(Construccion).
@@ -102,16 +92,16 @@ esTorre([X, Xs]) :-
     ancho(Xs, Value2),
     prof(X, Value3),
     prof(Xs, Value4),
-    nat_leq(Value1, Value2),
-    nat_leq(Value3, Value4).
+    lst_eq(Value1, Value2),
+    lst_eq(Value3, Value4).
 
 esTorre([X, Xs|Y]) :-
     ancho(X, Value1),
     ancho(Xs, Value2),
     prof(X, Value3),
     prof(Xs, Value4),
-    nat_leq(Value1, Value2),
-    nat_leq(Value3, Value4),
+    lst_eq(Value1, Value2),
+    lst_eq(Value3, Value4),
     esTorre([Xs|Y]).
 
 
@@ -176,7 +166,7 @@ esEdificioPiramide([]).
 esEdificioPiramide([X, Xs]) :-
     anchoEdificio(X, N),
     anchoEdificio(Xs, M),
-    nat_lt(N, M).
+    lst(N, M).
 
 anchoEdificio([], 0).
 anchoEdificio([X|Xs], N) :-
