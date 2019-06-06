efa09a348816d9c6628211d31fb184d65423e1f4
David Cristóbal <david@juanked.es>
2019-06-06 21:00:18 +0200

    Agregados números apartados

--- Practica2/codigo.pl
+++ Practica2/codigo.pl
@@ -3,10 +3,13 @@ alumno_prode(Benavente,Alvarez,Alejandro,160319).
 alumno_prode(Cristobal,Pascual,David,160336).
 alumno_prode(Doncel,Aparicio,Alberto,160364).   %PORTAVOZ
 %----------------------------------
+%1
 menor(A,B,Comp,M).
 
+%2
 menor_o_igual().
 
+%3
 ordenar(Lista,Comp,Orden).
 
 lista_hojas(Lista,Hojas).
