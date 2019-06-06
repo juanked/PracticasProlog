62323f05c50c66dd5355312d207bc673127bc23a
David Cristóbal <david@juanked.es>
2019-06-06 20:15:30 +0200

    Añadidos nombres predicados.

--- Practica2/codigo.pl
+++ Practica2/codigo.pl
@@ -2,3 +2,17 @@
 alumno_prode(Benavente,Alvarez,Alejandro,160319).
 alumno_prode(Cristobal,Pascual,David,160336).
 alumno_prode(Doncel,Aparicio,Alberto,160364).   %PORTAVOZ
+%----------------------------------
+menor(A,B,Comp,M).
+
+menor_o_igual().
+
+ordenar(Lista,Comp,Orden).
+
+lista_hojas(Lista,Hojas).
+
+hojas_arbol(Hojas, COmp, Arbol).
+
+ordenacion(Arbol,Comp,Orden).
+
+ordenar(Lista,Comp,Orden).
