f3d1e5849dff777a097fec3b0c12974ca9f12f84
palbertito <palbertito@hotmail.com>
2019-06-09 20:47:38 +0200

    1 hecho

--- Practica2/codigo.pl
+++ Practica2/codigo.pl
@@ -4,10 +4,21 @@ alumno_prode(Cristobal,Pascual,David,160336).
 alumno_prode(Doncel,Aparicio,Alberto,160364).   %PORTAVOZ
 %----------------------------------
 %1
-menor(A,B,Comp,M).
+menor(A,B,Comp,M):-
+    number(A),
+    number(B),
+    X=..[Comp,A,B],
+    call(X),
+    M is A.
 
+menor(A,B,Comp,M):-
+    number(A),
+    number(B),
+    X=..[Comp,B,A],
+    call(X),
+    M is B.
 %2
-menor_o_igual().
+menor_o_igual(A,B).
 
 %3
 ordenar(Lista,Comp,Orden).
