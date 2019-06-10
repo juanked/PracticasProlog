d3f5acf26d8fcfa8ea8438ac734d29496b14acbc
David Cristóbal <david@juanked.es>
2019-06-10 19:29:17 +0200

    Añadidos comentarios de 1 y 2

--- Practica2/codigo.pl
+++ Practica2/codigo.pl
@@ -3,52 +3,65 @@ alumno_prode(Benavente,Alvarez,Alejandro,160319).
 alumno_prode(Cristobal,Pascual,David,160336).
 alumno_prode(Doncel,Aparicio,Alberto,160364).   %PORTAVOZ
 %----------------------------------
-%1
-menor(A,B,Comp,M):-
-    number(A),
-    number(B),
-    X=..[Comp,A,B],
-    call(X),
-    M is A.
 
+%1
+
+% menor/4(num1, num2, Comp, Solucion) devuelve en M el menor entre A y B siguiendo Comp
+menor(A,B,Comp,M):-
+    number(A),          % Comprueba si A es un numero
+    number(B),          % Comprueba si B es un numero
+    X=..[Comp,A,B],     % Crea una función con Comp entre A y B
+    call(X),            % X es Goal
+    M is A.             % Guarda A en M si A es el menor
+
+% menor/4 devuelve en M el menor entre A y B siguiendo Comp
 menor(A,B,Comp,M):-
     number(A),
     number(B),
     X=..[Comp,B,A],
     call(X),
-    M is B.
+    M is B.             % Guarda B en M si B es el menor
+
 %2.
 
+% comp_rec/2(Lista1, Lista2)
+% true si cada elemento de la Lista1 es <= que el mismo elemento de la Lista2
 comp_rec([],[]).
-
 comp_rec([X|Xs],[Y|Ys]):-
     menor_o_igual(X,Y),
     comp_rec(Xs,Ys).
-    
+
+% menor_o_igual(Elemento1, Elemento2)
+% true si Elemento1 <= Elemento2 asi:
+%   variable libre es igual a otro termino
+%   un termino es menor que otro (ninguno variable libre)
+%   si su nombre es @< que el otro
+%   nombres identicos y aridad menor
+%   si nombre y aridad iguales se comparan de izquierda a derecha los terminos
 menor_o_igual(A,_):-
-    var(A).
+    var(A).             % comprueba que E1 es una variable
 
 menor_o_igual(_,A):-
-    var(A).
+    var(A).             % comprueba que E2 es una variable
 
 menor_o_igual(A,B):-
     functor(A,AF,_),
     functor(B,BF,_),
-    AF @< BF.
+    AF @< BF.           % comprueba si un el nombre de E1 es @< que el de E2
 
 menor_o_igual(A,B):-
     functor(A,_,AA),
     functor(B,_,BA),
-    AA < BA.
+    AA < BA.            % comprueba si la aridad de E1 es menor que la de E2
 
 menor_o_igual(A,B):-
     functor(A,AF,AA),
     functor(B,BF,BA),
-    AF == BF,
-    AA == BA,
-    A=..[_|X],
-    B=..[_|Y],
-    comp_rec(X,Y).
+    AF == BF,           % comprueba que los nombres de E1 y E2 sean iguales
+    AA == BA,           % comprueba que las aridades de E1 y E2 sean iguales
+    A=..[_|X],          % se quita la funcion para obtener el resto de elementos de E1
+    B=..[_|Y],          % se quita la funcion para obtener el resto de elementos de E2
+    comp_rec(X,Y).      % compara los elementos con com_rec/2.
 
 %3
 ordenar(Lista,Comp,Orden).
@@ -64,5 +77,3 @@ lista_hojas(Lista,Hojas):-
 hojas_arbol(Hojas, Comp, Arbol).
 
 ordenacion(Arbol,Comp,Orden).
-
-ordenar(Lista,Comp,Orden).
